c2e409f8f406ec7c88a9052d5c6338adb4de31ff
UnknownPgr <unknownpgr@gmail.com>
2024-09-13 04:17:30 +0900

    Add some servo options

--- src/arduino_pinmap.hpp
+++ src/arduino_pinmap.hpp
@@ -11,9 +11,11 @@
 #define PIN_BLDC 6
 #define PIN_SERVO_LEFT 9
 #define PIN_SERVO_RIGHT 10
+#define PIN_SERVO_T1 5
+#define PIN_SERVO_T2 3
 
-#define PIN_GPS_RX 4
-#define PIN_GPS_TX 3
+#define PIN_GPS_TX 0
+#define PIN_GPS_RX 1
 
 #define PIN_IMU_SDA // A4
 #define PIN_IMU_SCL // A5
